a0172556f810b37c55917d7220e6a661cf524548
Serega S <serega1067@yandex.ru>
2022-06-03 15:11:43 +0700

    Task 2 done

--- GB_homework_python_seminar4.py
+++ GB_homework_python_seminar4.py
@@ -12,6 +12,7 @@
 2 -> 10
 '''
 
+'''
 print("Переведём десятичное число в двоичное")
 decimal_number = int(input("Введите целое положительное "
                            "десяничное число: "))
@@ -27,6 +28,7 @@ def conversion_number(arg_num):
 
 result = conversion_number(decimal_number)
 print("Ответ: {}".format(result))
+'''
 
 '''
 Задача 2.
@@ -37,7 +39,40 @@ print("Ответ: {}".format(result))
 
 для k = 8 список будет выглядеть так: 
 [-21 ,13, -8, 5, -3, 2, -1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+'''
 
+print("Составим список из чисел Негафибоначчи и Фибоначчи")
+num = int(input("Введите целое число: "))
+res_list = []
+
+def num_negafib(arg_count, arg_num1, arg_num2, arg_res_list):
+    '''
+    Функция заполняет и выводит список из чисел Негафибоначчи
+    '''
+    while arg_count >= 0:
+        arg_count -= 1
+        num_negafib(
+            arg_count, arg_num2, arg_num1 - arg_num2, arg_res_list)
+        arg_res_list.append(arg_num1)
+        return arg_res_list
+
+
+def num_fib(arg_count, arg_num1, arg_num2, arg_res_list):
+    '''
+    Функция запускает функцию, которая заполняет список числами
+    Негафибоначчи, и после добавляет в этот же список числа Фибоначчи
+    '''
+    arg_res_list = num_negafib(
+        arg_count, arg_num1, arg_num2, arg_res_list)
+    for i in range(arg_count):
+        arg_num1, arg_num2 = arg_num2, arg_num1 + arg_num2
+        arg_res_list.append(arg_num1)
+    return arg_res_list
+
+res_list = num_fib(num, 0, 1, res_list)
+print("Ответ:", res_list)
+
+'''
 Задача 3.
 Задайте строку из набора чисел. Напишите программу, которая покажет 
 большее и меньшее число. В качестве символа-разделителя 
